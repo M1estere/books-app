eb4a4cb888b7697c09972639af64f7b03c03b956
Ilya_Solov <7156643@gmail.com>
2024-11-21 19:54:41 +0300

    Tests comments

--- tests/test_library.py
+++ tests/test_library.py
@@ -10,19 +10,19 @@ class TestLibrary(unittest.TestCase):
         self.library = Library('test_library.json')
 
     def test_add_book(self):
-        """Тестирование добавления книги"""
+        """Добавление книги"""
         self.library.add_book("Test Book", "Test Author", 2023)
         self.assertEqual(len(self.library.books), 1)
 
     def test_remove_book(self):
-        """Тестирование удаления книги"""
+        """Удаление книги"""
         self.library.add_book("Test Book", "Test Author", 2023)
         book_id = list(self.library.books.keys())[0]
         self.library.remove_book(book_id)
         self.assertEqual(len(self.library.books), 0)
 
     def test_remove_nonexistent_book(self):
-        """Тестирование удаления несуществующей книги"""
+        """Удаление несуществующей книги"""
         initial_count = len(self.library.books)
         with self.assertRaises(ValueError) as context:
             self.library.remove_book(999)
@@ -31,36 +31,36 @@ class TestLibrary(unittest.TestCase):
         self.assertEqual(len(self.library.books), initial_count)
 
     def test_search_books(self):
-        """Тестирование поиска книг"""
+        """Поиск книг"""
         self.library.add_book("Test Book", "Test Author", 2023)
         results = self.library.search_books("Test Book")
         self.assertEqual(len(results), 1)
 
     def test_search_books_by_author(self):
-        """Тестирование поиска по автору"""
+        """Поиск по автору"""
         self.library.add_book("Another Book", "Test Author", 2023)
         results = self.library.search_books("Test Author")
         self.assertEqual(len(results), 1)
 
     def test_search_books_by_year(self):
-        """Тестирование поиска по году"""
+        """Поиск по году"""
         self.library.add_book("Year Book", "Some Author", 2023)
         results = self.library.search_books(2023)
         self.assertEqual(len(results), 1)
 
     def test_display_books_empty(self):
-        """Тестирование отображения книг в пустой библиотеке"""
+        """Отображение книг в пустой библиотеке"""
         self.library.display_books()
 
     def test_change_status(self):
-        """Тестирование изменения статуса книги"""
+        """Изменение статуса книги"""
         self.library.add_book("Status Book", "Test Author", 2023)
         book_id = list(self.library.books.keys())[0]
         self.library.change_status(book_id, "выдана")
         self.assertEqual(self.library.books[book_id].status, "выдана")
 
     def test_change_status_nonexistent_book(self):
-        """Тестирование изменения статуса несуществующей книги"""
+        """Изменение статуса несуществующей книги"""
         self.library.add_book("Status Book", "Test Author", 2023)
         book_id = list(self.library.books.keys())[0]
 
@@ -72,7 +72,7 @@ class TestLibrary(unittest.TestCase):
         self.assertEqual(self.library.books[book_id].status, "выдана")
 
     def test_change_status_invalid_status(self):
-        """Тестирование изменения статуса с некорректным значением"""
+        """Изменение статуса с некорректным значением"""
         self.library.add_book("Invalid Status Book", "Test Author", 2023)
         book_id = list(self.library.books.keys())[0]
         with self.assertRaises(ValueError) as context:
